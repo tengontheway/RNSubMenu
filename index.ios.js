de7ab652c9f67c4c9d2caeffbde06c888ad3d42c
tzt <teng_ontheway@163.com>
2016-06-27 09:05:14 +0800

    实现了一般的动态二级菜单,todo

--- index.ios.js
+++ index.ios.js
@@ -2,6 +2,8 @@
  * 二级菜单组件
  * 1.采用动态style
  * 2.根据点击动态切换显示ScrollView
+ * 3.接口越简单越好
+ * 参考: https://github.com/vczero/react-native-tab-menu
  */
 
 import React, { Component } from 'react';
@@ -15,19 +17,84 @@ import {
   ScrollView
 } from 'react-native';
 
-class MenuList extends Component {
 
+var data = {
+  "全部区域": {
+    "全部区域": ["全部区域"],
+    "热门商圈": [
+      "虹桥地区",
+      "徐家汇地区",
+      "东单商业区",
+      "西单商业区",
+      "王府井商业区",
+    ],
+    "热门行政区": [
+      "东单",
+      "西单",
+      "天通苑南",
+      "天通苑北",
+      "夏家胡同",
+    ]
+  },
+  "地铁沿线": {
+    "地铁全线": ["地铁全线"],
+    "一号线": ["土桥", "东单", "西单", "公主坟", "军博"],
+    "二号线": ["西苑", "中关村", "人民大学", "国家图书馆"],
+  }
+};
+
+var prefixType = '_type_';
+var prefixStyle = '_style_';
+var defaultBackgroundColor = {backgroundColor: '#fff' }
+
+class MenuList extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      wholeArea: false,
-      hotBusiness: true,
-      hotDistrict: false,
-      wholeAreaFFF: {},
-      hotBusinessFFF: { backgroundColor: '#fff' },
-      hotDistricFFF: {}
+    var data = this.props.data;
+
+    //选中的头部idx
+    var tabSelected = this.props.tabSelected;
+
+    var subSelected = this.props.subSelected;
+
+    var obj = {};
+
+    var kindex = 0;
+
+    for (var k in data) {
+      var childData = data[k];
+      var cindex = 0;
+
+      for (var c in childData) {
+        var type = prefixType + k + '_' + c;
+        var style = prefixStyle + k + '_' + c;
+
+        obj[type] = false;
+        obj[style] = {};
+
+        // 选定的类型
+        if (tabSelected === kindex && subSelected === cindex) {
+          obj[type] = true;
+          obj[style] = defaultBackgroundColor;
+        }
+
+        cindex++;
+      }
+
+      kindex++;
     }
+
+    this.state = obj;
+
+    // this.state = {
+    //   wholeArea: false,
+    //   hotBusiness: true,
+    //   hotDistrict: false,
+    //   wholeAreaFFF: {},
+    //   hotBusinessFFF: { backgroundColor: '#fff' },
+    //   hotDistricFFF: {}
+    // }
   }
 
   render() {
@@ -56,34 +123,34 @@ class MenuList extends Component {
           </ScrollView>
 
           {
-            this.state.wholeArea?
-            <ScrollView style={[styles.flex_1, styles.right_panel]}>
-              <Text style={styles.left_row}>全部地区</Text>
-            </ScrollView>
-            : null
+            this.state.wholeArea ?
+              <ScrollView style={[styles.flex_1, styles.right_panel]}>
+                <Text style={styles.left_row}>全部地区</Text>
+              </ScrollView>
+              : null
           }
 
           {
-            this.state.hotBusiness?
-            <ScrollView style={[styles.flex_1, styles.right_panel]}>
-              <Text style={styles.left_row}>西直门</Text>
-              <Text style={styles.left_row}>东直门</Text>
-              <Text style={styles.left_row}>东单</Text>
-              <Text style={styles.left_row}>西单</Text>
-            </ScrollView>
-            : null
+            this.state.hotBusiness ?
+              <ScrollView style={[styles.flex_1, styles.right_panel]}>
+                <Text style={styles.left_row}>西直门</Text>
+                <Text style={styles.left_row}>东直门</Text>
+                <Text style={styles.left_row}>东单</Text>
+                <Text style={styles.left_row}>西单</Text>
+              </ScrollView>
+              : null
           }
 
           {
-            this.state.hotDistrict?
-            <ScrollView style={[styles.flex_1, styles.right_panel]}>
-              <Text style={styles.left_row}>北京</Text>
-              <Text style={styles.left_row}>上海</Text>
-              <Text style={styles.left_row}>广州</Text>
-              <Text style={styles.left_row}>深圳</Text>
-              <Text style={styles.left_row}>成都</Text>
-            </ScrollView>
-            : null
+            this.state.hotDistrict ?
+              <ScrollView style={[styles.flex_1, styles.right_panel]}>
+                <Text style={styles.left_row}>北京</Text>
+                <Text style={styles.left_row}>上海</Text>
+                <Text style={styles.left_row}>广州</Text>
+                <Text style={styles.left_row}>深圳</Text>
+                <Text style={styles.left_row}>成都</Text>
+              </ScrollView>
+              : null
           }
         </View>
 
@@ -102,95 +169,99 @@ class MenuList extends Component {
     });
   }
 
-    hotBusiness() {
-      this.setState({
-        wholeArea: false,
-        hotBusiness: true,
-        hotDistrict: false,
-        wholeAreaFFF: {},
-        hotBusinessFFF: { backgroundColor: '#fff' },
-        hotDistricFFF: {}
-      });
-    }
-
-      hotDistrict() {
-        this.setState({
-          wholeArea: false,
-          hotBusiness: false,
-          hotDistrict: true,
-          wholeAreaFFF: {},
-          hotBusinessFFF: {},
-          hotDistricFFF: { backgroundColor: '#fff' }
-        });
-      }
-    }
-
-
-    class App extends Component {
-
-      // 注意: 带参数回调函数的使用navChange
-      render() {
-        return (
-          <View style = {styles.margin_top}>
-            <MenuList />
-          </View>
-        );
-      }
-
-    }
-
-    const styles = StyleSheet.create({
-      container: {
-        flex: 1,
-        flexDirection: 'column',
-        borderTopWidth: 1,
-        borderBottomWidth: 1,
-        borderColor: '#ddd',
-        height: 240,
-      },
-      margin_top: {
-        marginTop: 27,
-      },
-      flex_1: {
-        flex: 1,
-      },
-      flex_row: {
-        flexDirection: 'row',
-      },
-      text_head: {
-        color: '#7b7b7b',
-        textAlign: 'center',
-        fontSize: 15,
-      },
-      height_head: {
-        height: 35,
-        borderBottomWidth: 1,
-        backgroundColor: '#b5b5b5',
-        borderBottomColor: '#f5f5f5',
-      },
-      head_container: {
-        justifyContent: 'center',
-        alignItems: 'center',
-      },
-      left_panel: {
-        backgroundColor: '#f2f2f2',
-      },
-      right_panel: {
-        marginLeft: 10,
-      },
-      left_row: {
-        height: 30,
-        lineHeight: 20,
-        fontSize: 14,
-        color: '#7c7c7c',
-      },
-      active_blue: {
-        color: '#00B7EB',
-      },
-      active_fff: {
-        backgroundColor: '#fff',
-      }
+  hotBusiness() {
+    this.setState({
+      wholeArea: false,
+      hotBusiness: true,
+      hotDistrict: false,
+      wholeAreaFFF: {},
+      hotBusinessFFF: { backgroundColor: '#fff' },
+      hotDistricFFF: {}
     });
+  }
+
+  hotDistrict() {
+    this.setState({
+      wholeArea: false,
+      hotBusiness: false,
+      hotDistrict: true,
+      wholeAreaFFF: {},
+      hotBusinessFFF: {},
+      hotDistricFFF: { backgroundColor: '#fff' }
+    });
+  }
+}
 
 
-    AppRegistry.registerComponent('RNSubMenu', () => App);
+class App extends Component {
+
+  // 注意: 带参数回调函数的使用navChange
+  render() {
+    return (
+      <View style = {styles.margin_top}>
+        <MenuList data={data} tabSelected={0} subSelected={1} click={this.onPress} />
+      </View>
+    );
+  }
+
+  onPress(val) {
+    alert(val);
+  }
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    borderColor: '#ddd',
+    height: 240,
+  },
+  margin_top: {
+    marginTop: 27,
+  },
+  flex_1: {
+    flex: 1,
+  },
+  flex_row: {
+    flexDirection: 'row',
+  },
+  text_head: {
+    color: '#7b7b7b',
+    textAlign: 'center',
+    fontSize: 15,
+  },
+  height_head: {
+    height: 35,
+    borderBottomWidth: 1,
+    backgroundColor: '#b5b5b5',
+    borderBottomColor: '#f5f5f5',
+  },
+  head_container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  left_panel: {
+    backgroundColor: '#f2f2f2',
+  },
+  right_panel: {
+    marginLeft: 10,
+  },
+  left_row: {
+    height: 30,
+    lineHeight: 20,
+    fontSize: 14,
+    color: '#7c7c7c',
+  },
+  active_blue: {
+    color: '#00B7EB',
+  },
+  active_fff: {
+    backgroundColor: '#fff',
+  }
+});
+
+
+AppRegistry.registerComponent('RNSubMenu', () => App);
